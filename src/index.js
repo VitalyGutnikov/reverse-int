fa4dd2e45a3ff0b8ccbb99f41e98c810eaf6052d
Виталий Гутников <vitaly_vg.st_prod@mail.ru>
2022-08-05 22:28:01 +0300

    completed reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+	let i = 0;
+    n = Math.abs(n);
+	n = n.toString();
+	let result = '';
+    
+	while (i < n.length){
+	  result = n[i] + result;
+      console.log(result);
+	  i++;
+	}
+
+    result= +result;
+	return result;
 }
